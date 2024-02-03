2b6f5d153c9e4e8aa38a460d7203d9c5ed2e6f4f
zahangir710 <zahangir710@gmail.com>
2024-02-03 02:18:00 +0100

    add sidebar to main-layout

--- src/Layouts/MainLayout/MainLayout.jsx
+++ src/Layouts/MainLayout/MainLayout.jsx
@@ -1,13 +1,19 @@
 import { Outlet } from "react-router-dom";
 import Header from "../../shared/Header/Header";
 import Footer from "../../shared/Footer/Footer";
+import "./MainLayout.css";
 
 function MainLayout() {
   return (
-    <div>
-      <Header></Header>
-      <Outlet></Outlet>
-      <Footer></Footer>
+    <div className="main-layout">
+      <div className="sidebar">
+        <p>Left Sidebar</p>
+      </div>
+      <div className="content">
+        <Header />
+        <Outlet />
+        <Footer />
+      </div>
     </div>
   );
 }
