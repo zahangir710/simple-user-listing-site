b669510e50c819be8cdacaecc0e89d5f41c7d3fe
zahangir710 <zahangir710@gmail.com>
2024-02-03 16:26:40 +0100

    load user data

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,22 @@
+import { useEffect, useState } from "react";
+import Card from "../../components/Card/Card";
 import "./Home.css";
 function Home() {
-  return <div>This is home</div>;
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users").then((res) =>
+      res.json().then((data) => setUsers(data))
+    );
+  }, []);
+  console.log(users);
+  return (
+    <div className="card-container ">
+      {users.map((user) => (
+        <Card user={user} key={user.id}></Card>
+      ))}
+    </div>
+  );
 }
 
 export default Home;
