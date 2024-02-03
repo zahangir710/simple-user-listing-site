f920a5175121ed91c557fb5a49c9c27704dc5ea0
zahangir710 <zahangir710@gmail.com>
2024-02-03 01:44:36 +0100

    add header component

--- src/shared/Header/Header.jsx
+++ src/shared/Header/Header.jsx
@@ -1,6 +1,29 @@
+import { Link } from "react-router-dom";
 import "./Header.css";
 function Header() {
-  return <div>This is the header</div>;
+  return (
+    <header className="header">
+      <div className="logo">
+        <Link to="/">Meet Friends</Link>
+      </div>
+      <nav className="navbar">
+        <ul className="nav-list">
+          <li className="nav-item">
+            <Link to="/discover">Discover</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/add">Add</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/login">Login</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/">Logout</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
