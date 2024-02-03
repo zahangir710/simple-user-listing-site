deca3be828d337de2afa4527b38bbe4e42fcd3e2
zahangir710 <zahangir710@gmail.com>
2024-02-03 01:43:13 +0100

    set base styles

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <div className="screen-size">
+      <RouterProvider router={router} />
+    </div>
   </React.StrictMode>
 );
